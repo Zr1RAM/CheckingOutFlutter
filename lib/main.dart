9bddcab5b438aa8dda7a0e09a64d1db648a1eb3a
Zr1RAM <sriram.varadarajulu@gmail.com>
2021-07-21 00:44:35 +0530

    Containers padding and margins

--- lib/main.dart
+++ lib/main.dart
@@ -16,18 +16,12 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body: Center(
-        child: Text(
-          '6 packs coming soon',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.blueGrey,
-            fontFamily: 'IndieFlower',
-          ),
-          ),
-      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0), // There is fromLTRB, Symmetric , etc
+        margin: EdgeInsets.symmetric(horizontal: 155.0, vertical: 10.0),
+        color: Colors.grey,
+        child: Text('Text inside a container'),
+        ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red,
         onPressed: () {},
